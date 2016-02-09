df797587e0ab89a67a945c6f6a6edeca67021372
Ben Harold <benharold@mac.com>
2016-02-08 21:41:24 -0500

    don't need locationManager for hello world

--- objective-c/README.md
+++ objective-c/README.md
@@ -1,5 +1,5 @@
 # Objective-C Hello World
 
 * The source code is in `world.m`
-* Compile it: `gcc world.m -o HelloWorld -ObjC -framework Foundation -framework CoreLocation`
+* Compile it: `gcc world.m -o HelloWorld -ObjC -framework Foundation`
 * Run it: `./HelloWorld`
--- objective-c/world.m
+++ objective-c/world.m
@@ -1,10 +1,8 @@
 #import <Foundation/Foundation.h>
-#import <CoreLocation/CoreLocation.h>
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    CLLocationManager * locationManager = [[CLLocationManager alloc] init];
     NSLog(@"hello world");
 
     [pool drain];
